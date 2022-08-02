d4d1d6c0bd6dc16b2669da6d6a1861feb1b837a6
“antoanparashkevov” <tonkata1505@gmail.com>
2022-08-02 15:34:16 +0300

    Test if our regex pattern works correctly

--- engine.js
+++ engine.js
@@ -1,6 +1,10 @@
-export function createTemplate(templateAsString){
-const pattern = /{{(.+?)}}/g
-}
-export function renderTemplate(template,data){
+export function createTemplate(templateAsString) {
+    const pattern = /{{(.+?)}}/g
+
+    return (data)=>templateAsString.replace(pattern,(match,group)=>{
+        console.log(group)
+        return match;//will return whole article
+    })
 
 }
+
