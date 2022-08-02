93e619c79029e5280915793ee5d5dec91dd1c15c
“antoanparashkevov” <tonkata1505@gmail.com>
2022-08-02 15:37:42 +0300

    Test a fetch request for rendering article template

--- app.js
+++ app.js
@@ -2,29 +2,35 @@ console.log('test')
 
 import {data, nav as links} from './data.js'
 
+import {createTemplate} from "./engine.js";
+
 
 start()
 
-function start(){
+async function start(){
     const main = document.querySelector('main')
-    main.innerHTML = data.map(article=>articleTemplate(article)).join('')
+    // main.innerHTML = data.map(article=>articleTemplate(article)).join('')
     const navTag= document.querySelector('nav ul')
-    navTag.innerHTML = links.map(nav => navTemplate(nav)).join('')
+    // navTag.innerHTML = links.map(nav => navTemplate(nav)).join('')
 
-    console.log(data.map(article=>articleTemplate(article)))
-}
-function articleTemplate(article){
+    const articleTemplateAsString = await (await fetch('./templates/article.html')).text()
+    console.log(articleTemplateAsString)
 
-    return `<article>
-        <h1>${article.title}</h1>
-        <div class="content">
-            <p>${article.content}
-            </p>
-        </div>
-        <footer>Author: ${article.author}</footer>
-    </article>`
-}
 
-function navTemplate(nav){
-    return `<li><a href="${nav.href}">${nav.label}</a>`
+    // console.log(data.map(article=>articleTemplate(article)))
 }
+// function articleTemplate(article){
+//
+//     return `<article>
+//         <h1>${article.title}</h1>
+//         <div class="content">
+//             <p>${article.content}
+//             </p>
+//         </div>
+//         <footer>Author: ${article.author}</footer>
+//     </article>`
+// }
+//
+// function navTemplate(nav){
+//     return `<li><a href="${nav.href}">${nav.label}</a>`
+// }
