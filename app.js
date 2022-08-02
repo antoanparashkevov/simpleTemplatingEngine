5b5722a8d4cf9b085eec5e6a4e678e74e1d27b1b
“antoanparashkevov” <tonkata1505@gmail.com>
2022-08-02 14:53:33 +0300

    Using interpolated string, outputting data dynamically

--- app.js
+++ app.js
@@ -2,21 +2,22 @@ console.log('test')
 
 import {data} from './data.js'
 
-const articleAsHtml = ` <article>
-        <h1>First Article</h1>
-        <div class="content">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At quam, sed? Aliquam animi asperiores at
-                cumque debitis dignissimos distinctio dolor dolore ducimus, esse eum fuga fugiat illum impedit ipsum
-                iure iusto labore maiores minus molestias necessitatibus numquam odit officiis optio, quae quaerat qui
-                quisquam quos reprehenderit sequi tempore ut voluptate, voluptates?
-            </p>
-        </div>
-        <footer>Author: John Smith</footer>
-    </article>`
 
 start()
 
 function start(){
     const main = document.querySelector('main')
+   console.log( data.map(article=>articleTemplate(article)))
     main.innerHTML = 'Hello'
+}
+function articleTemplate(article){
+
+    return `<article>
+        <h1>${article.title}</h1>
+        <div class="content">
+            <p>${article.content}
+            </p>
+        </div>
+        <footer>Author: ${article.author}</footer>
+    </article>`
 }
