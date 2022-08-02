c941bdf9a18aa02d4b93d90f49bc33dfbc7d9daf
“antoanparashkevov” <tonkata1505@gmail.com>
2022-08-02 15:42:28 +0300

    Returns real data from our regex match

--- engine.js
+++ engine.js
@@ -2,8 +2,9 @@ export function createTemplate(templateAsString) {
     const pattern = /{{(.+?)}}/g
 
     return (data)=>templateAsString.replace(pattern,(match,group)=>{
-        console.log(group)
-        return match;//will return whole article
+        // console.log(group)
+        // return match;//will return whole article
+        return data[group]
     })
 
 }
